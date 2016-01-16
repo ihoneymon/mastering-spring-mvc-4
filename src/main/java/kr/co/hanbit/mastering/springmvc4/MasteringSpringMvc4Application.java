8f3bb5a90197c0d65ec53ae9dbd9eb391e128203
honeymon <ihoneymon@gmail.com>
2016-01-16 15:01:47 +0900

    Init Project: Mastering Spring MVC 4

---
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/MasteringSpringMvc4Application.java
@@ -0,0 +1,12 @@
+package kr.co.hanbit.mastering.springmvc4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringMvc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpringMvc4Application.class, args);
+	}
+}
---
+++ src/test/java/kr/co/hanbit/mastering/springmvc4/MasteringSpringMvc4ApplicationTests.java
@@ -0,0 +1,18 @@
+package kr.co.hanbit.mastering.springmvc4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MasteringSpringMvc4Application.class)
+@WebAppConfiguration
+public class MasteringSpringMvc4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
