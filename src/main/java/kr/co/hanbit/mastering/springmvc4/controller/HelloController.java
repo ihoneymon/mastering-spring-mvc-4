528ba1177def5e36143a1cc187eb12df2d9cb7f8
honeymon <ihoneymon@gmail.com>
2016-01-16 16:55:32 +0900

    Controller add model 추가

--- src/main/java/kr/co/hanbit/mastering/springmvc4/controller/HelloController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/controller/HelloController.java
@@ -1,13 +1,15 @@
 package kr.co.hanbit.mastering.springmvc4.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello from the controller");
         return "hello";
     }
 }
