cc113ca7f11d89800c3f9c06d77672ca59318a6e
honeymon <ihoneymon@gmail.com>
2016-01-16 15:39:22 +0900

    HelloController 추가

---
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/controller/HelloController.java
@@ -0,0 +1,15 @@
+package kr.co.hanbit.mastering.springmvc4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello, world!";
+    }
+}
