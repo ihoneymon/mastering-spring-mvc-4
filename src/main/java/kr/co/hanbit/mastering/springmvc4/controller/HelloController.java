672846522ded00989dc5105aaf87c6600469e8b0
honeymon <ihoneymon@gmail.com>
2016-01-16 18:10:48 +0900

    Add userName parameter at HelloController

--- src/main/java/kr/co/hanbit/mastering/springmvc4/controller/HelloController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/controller/HelloController.java
@@ -3,13 +3,14 @@ package kr.co.hanbit.mastering.springmvc4.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello from the controller");
+    public String hello(@RequestParam("name") String userName, Model model) {
+        model.addAttribute("message", "Hello, " + userName);
         return "hello";
     }
 }
