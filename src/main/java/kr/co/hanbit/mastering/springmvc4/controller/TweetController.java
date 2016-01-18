cbc3cf9bdeb5ad4b2672cb83f702ac4df9d387c0
honeymon <ihoneymon@gmail.com>
2016-01-18 23:59:08 +0900

    Chagne: HelloController -> TweetController, hello.html -> tweets.html

--- src/main/java/kr/co/hanbit/mastering/springmvc4/controller/TweetController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/controller/TweetController.java
@@ -1,16 +1,28 @@
 package kr.co.hanbit.mastering.springmvc4.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class TweetController {
+
+    @Autowired
+    private Twitter twitter;
 
     @RequestMapping("/")
-    public String hello(@RequestParam(defaultValue = "world") String name, Model model) {
-        model.addAttribute("message", "Hello, " + name);
-        return "hello";
+    public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
+        model.addAttribute("tweets", tweets);
+        return "tweets";
     }
 }
