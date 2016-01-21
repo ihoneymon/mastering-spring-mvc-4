d546762add6b413847eba7b14085056b7db07043
honeymon <ihoneymon@gmail.com>
2016-01-21 17:40:43 +0900

    search-page 폼 요청방식을 GET에서 POST로 변경

--- src/main/java/kr/co/hanbit/mastering/springmvc4/controller/TweetController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/controller/TweetController.java
@@ -2,6 +2,8 @@ package kr.co.hanbit.mastering.springmvc4.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -9,7 +11,9 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TweetController {
@@ -23,11 +27,18 @@ public class TweetController {
     }
 
     @RequestMapping("/result")
-    public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
+    public String searchResult(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
         SearchResults searchResults = twitter.searchOperations().search(search);
         List<Tweet> tweets = searchResults.getTweets();
         model.addAttribute("tweets", tweets);
         model.addAttribute("search", search);
         return "result-page";
+    }
+
+    @RequestMapping(value = "/post-search", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        String search = request.getParameter("search");
+        redirectAttributes.addAttribute("search", search);
+        return "redirect:result";
     }
 }
