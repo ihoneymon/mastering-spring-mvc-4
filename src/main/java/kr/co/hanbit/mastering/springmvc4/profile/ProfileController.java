507466084bc5e4ee69f718da7b39ff72a362bc7e
honeymon <ihoneymon@gmail.com>
2016-01-23 12:19:10 +0900

    ProfileController 추가

---
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/profile/ProfileController.java
@@ -0,0 +1,14 @@
+package kr.co.hanbit.mastering.springmvc4.profile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProfileController {
+
+    @RequestMapping("/profile")
+    public String displayProfile() {
+        return "profile/profilePage";
+    }
+
+}
