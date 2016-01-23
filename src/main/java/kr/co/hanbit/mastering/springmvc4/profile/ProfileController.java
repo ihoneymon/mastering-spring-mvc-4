d5c5f8010762ee2105301f59a83c9619975c308f
honeymon <ihoneymon@gmail.com>
2016-01-23 16:54:17 +0900

    ProfileController POST 메서드 추가

--- src/main/java/kr/co/hanbit/mastering/springmvc4/profile/ProfileController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/profile/ProfileController.java
@@ -2,13 +2,20 @@ package kr.co.hanbit.mastering.springmvc4.profile;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProfileController {
 
     @RequestMapping("/profile")
-    public String displayProfile() {
+    public String displayProfile(ProfileForm profileForm) {
         return "profile/profile-page";
     }
 
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    public String saveProfile(ProfileForm profileForm) {
+        System.out.println("save ok" + profileForm);
+        return "redirect:/profile";
+    }
+    
 }
