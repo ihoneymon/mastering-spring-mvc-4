5fe4a5c140c7d85c972a2c7f72a432f09dfd3a53
honeymon <ihoneymon@gmail.com>
2016-01-24 23:00:57 +0900

    ProfileForm 에 대한 검증validation 애노테이션 추가

--- src/main/java/kr/co/hanbit/mastering/springmvc4/profile/ProfileForm.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/profile/ProfileForm.java
@@ -4,14 +4,28 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import lombok.Data;
 
 @Data
 public class ProfileForm {
 
+    @Size(min = 2)
     private String twitterHandle;
+    
+    @Email
+    @NotEmpty
     private String email;
+    
+    @NotNull
     private LocalDate birthDate;
+    
+    @NotEmpty
     private List<String> tastes = new ArrayList<>();
 
 }
