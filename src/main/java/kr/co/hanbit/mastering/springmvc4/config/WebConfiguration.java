98d5d6b3ed5de8112b0fa8d220fbceaae78e2059
honeymon <ihoneymon@gmail.com>
2016-01-26 07:50:07 +0900

    LocaleResolver 설정 추가

--- src/main/java/kr/co/hanbit/mastering/springmvc4/config/WebConfiguration.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/config/WebConfiguration.java
@@ -2,9 +2,14 @@ package kr.co.hanbit.mastering.springmvc4.config;
 
 import java.time.LocalDate;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import kr.co.hanbit.mastering.springmvc4.date.KRLocalDateFormatter;
 
@@ -14,5 +19,22 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatterForFieldType(LocalDate.class, new KRLocalDateFormatter());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
