02742336979fca65dfa64fb097e941ef5630b089
honeymon <ihoneymon@gmail.com>
2016-01-26 23:36:49 +0900

    profile-page 기호 추가기능 구현

--- src/main/java/kr/co/hanbit/mastering/springmvc4/profile/ProfileController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/profile/ProfileController.java
@@ -2,6 +2,7 @@ package kr.co.hanbit.mastering.springmvc4.profile;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -27,13 +28,26 @@ public class ProfileController {
         return "profile/profile-page";
     }
 
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    @RequestMapping(value = "/profile", params = { "save" }, method = RequestMethod.POST)
     public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             return "profile/profile-page";
         }
         log.debug("ProfileForm: {}", profileForm);
         return "redirect:/profile";
+    }
+
+    @RequestMapping(value = "/profile", params = { "addTaste" })
+    public String addRow(ProfileForm profileForm) {
+        profileForm.getTastes().add(null);
+        return "profile/profile-page";
+    }
+
+    @RequestMapping(value = "/profile", params = { "removeTaste" })
+    public String removeRow(ProfileForm profileForm, HttpServletRequest req) {
+        Integer rowId = Integer.valueOf(req.getParameter("removeTaste"));
+        profileForm.getTastes().remove(rowId.intValue());
+        return "profile/profile-page";
     }
 
 }
