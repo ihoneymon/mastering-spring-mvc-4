ee42bebf96299ad123394602fefe0e9be97c5236
honeymon <ihoneymon@gmail.com>
2016-02-05 22:16:31 +0900

    MultipartFile 복사 기능 구현

--- src/main/java/kr/co/hanbit/mastering/springmvc4/profile/PicutureUploadController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/profile/PicutureUploadController.java
@@ -1,6 +1,10 @@
 package kr.co.hanbit.mastering.springmvc4.profile;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.core.io.FileSystemResource;
@@ -9,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class PicutureUploadController {
@@ -21,13 +26,28 @@ public class PicutureUploadController {
     }
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String onUpload(MultipartFile file) throws IOException {
+    public String onUpload(MultipartFile file, RedirectAttributes redirectAttrs) throws IOException {
+
+        if (file.isEmpty() || !isImage(file)) {
+            redirectAttrs.addFlashAttribute("error", "Incorrect file.Please upload a picture.");
+            return "redirect:/upload";
+        }
+
+        copyFileToPictures(file);
+
+        return "profile/upload-page";
+    }
+
+    private void copyFileToPictures(MultipartFile file) throws IOException {
         String filename = file.getOriginalFilename();
         File tempFile = File.createTempFile("pic", getFileExtension(filename), PICTURES_DIR.getFile());
         try (InputStream in = file.getInputStream(); OutputStream out = new FileOutputStream(tempFile)) {
             IOUtils.copy(in, out);
         }
-        return "profile/upload-page";
+    }
+
+    private boolean isImage(MultipartFile file) {
+        return file.getContentType().startsWith("image");
     }
 
     private static String getFileExtension(String name) {
