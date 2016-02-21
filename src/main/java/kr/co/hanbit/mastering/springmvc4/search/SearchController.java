e452e1145c256281b10d7c7b7fde55b7ea99f7f8
honeymon <ihoneymon@gmail.com>
2016-02-21 17:36:47 +0900

    트위터 검색기능 구현

--- src/main/java/kr/co/hanbit/mastering/springmvc4/search/SearchController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/search/SearchController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.Tweet;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class SearchController {
 
     private SearchService searchService;
--- src/main/java/kr/co/hanbit/mastering/springmvc4/search/SearchService.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/search/SearchService.java
@@ -1,8 +1,10 @@
 package kr.co.hanbit.mastering.springmvc4.search;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchParameters;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Service;
@@ -18,7 +20,31 @@ public class SearchService {
     }
 
     public List<Tweet> search(String searchType, List<String> keywords) {
-        return null;
+        List<SearchParameters> searches = keywords.stream()
+                .map(taste -> createSearchParam(searchType, taste))
+                .collect(Collectors.toList());
+        
+        List<Tweet> tweets = searches.stream()
+                .map(params -> twitter.searchOperations().search(params))
+                .flatMap(searchResults -> searchResults.getTweets().stream())
+                .collect(Collectors.toList());
+        return tweets;
     }
 
+    private SearchParameters.ResultType getResultType(String searchType) {
+        for (SearchParameters.ResultType knownType : SearchParameters.ResultType.values()) {
+            if (knownType.name().equalsIgnoreCase(searchType)) {
+                return knownType;
+            }
+        }
+        return SearchParameters.ResultType.RECENT;
+    }
+
+    private SearchParameters createSearchParam(String searchType, String taste) {
+        SearchParameters.ResultType resultType = getResultType(searchType);
+        SearchParameters searchParameters = new SearchParameters(taste);
+        searchParameters.resultType(resultType);
+        searchParameters.count(3);
+        return searchParameters;
+    }
 }
