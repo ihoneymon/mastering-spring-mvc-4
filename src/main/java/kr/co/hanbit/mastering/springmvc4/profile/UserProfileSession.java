452428a88b7d30c83d319fd5d09cb116578bf69d
honeymon <ihoneymon@gmail.com>
2016-02-21 15:24:44 +0900

    UserProfileSession 클래스 추가

---
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/profile/UserProfileSession.java
@@ -0,0 +1,36 @@
+package kr.co.hanbit.mastering.springmvc4.profile;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class UserProfileSession implements Serializable {
+
+    private String twitterHandle;
+    private String email;
+    private LocalDate birthDate;
+    private List<String> tastes = new ArrayList<>();
+
+    public void saveForm(ProfileForm profileForm) {
+        this.twitterHandle = profileForm.getTwitterHandle();
+        this.email = profileForm.getEmail();
+        this.birthDate = profileForm.getBirthDate();
+        this.tastes = profileForm.getTastes();
+    }
+
+    public ProfileForm toForm() {
+        ProfileForm profileForm = new ProfileForm();
+        profileForm.setTwitterHandle(twitterHandle);
+        profileForm.setEmail(email);
+        profileForm.setBirthDate(birthDate);
+        profileForm.setTastes(tastes);
+        return profileForm;
+    }
+}
