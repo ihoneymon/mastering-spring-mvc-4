881123226fd776b3506a2544c32ac295bfc9a949
honeymon <ihoneymon@gmail.com>
2016-03-04 23:39:13 +0900

    SearchApiController 추가

---
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/search/ai/SearchApiController.java
@@ -0,0 +1,30 @@
+package kr.co.hanbit.mastering.springmvc4.search.ai;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.web.bind.annotation.MatrixVariable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.hanbit.mastering.springmvc4.search.SearchService;
+
+@RestController
+@RequestMapping("/api/search")
+public class SearchApiController {
+
+    private SearchService searchService;
+    
+    @Autowired
+    public SearchApiController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+    
+    @RequestMapping(value="/{searchType}", method=RequestMethod.GET)
+    public List<Tweet> search(@PathVariable String searchType, @MatrixVariable List<String> keywords) {
+        return searchService.search(searchType, keywords);
+    }
+}
