d2c1ae926b1b2959a5da85da6201258cdaf09fd7
honeymon <ihoneymon@gmail.com>
2016-03-14 22:50:23 +0900

    UserRepository 에서 예외처리

--- src/main/java/kr/co/hanbit/mastering/springmvc4/user/UserRepository.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/user/UserRepository.java
@@ -7,10 +7,20 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Repository;
 
+import kr.co.hanbit.mastering.springmvc4.error.EntityNotFoundException;
+
 @Repository
 public class UserRepository {
 
     private final Map<String, User> userMap = new ConcurrentHashMap<>();
+
+    public User update(String email, User user) throws EntityNotFoundException {
+        if (!exists(email)) {
+            throw new EntityNotFoundException("User " + email + "cannot be found");
+        }
+        user.setEmail(email);
+        return userMap.put(email, user);
+    }
 
     public User save(String email, User user) {
         user.setEmail(email);
@@ -21,7 +31,10 @@ public class UserRepository {
         return save(user.getEmail(), user);
     }
 
-    public User findOne(String email) {
+    public User findOne(String email) throws EntityNotFoundException {
+        if (!exists(email)) {
+            throw new EntityNotFoundException("User " + email + "cannot be found");
+        }
         return userMap.get(email);
     }
 
@@ -29,7 +42,10 @@ public class UserRepository {
         return new ArrayList<>(userMap.values());
     }
 
-    public void delete(String email) {
+    public void delete(String email) throws EntityNotFoundException {
+        if (!exists(email)) {
+            throw new EntityNotFoundException("User " + email + "cannot be found");
+        }
         userMap.remove(email);
     }
 
