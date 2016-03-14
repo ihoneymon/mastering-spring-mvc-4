767d66e6f2dc636f8d289be84bb8795bcab83de5
honeymon <ihoneymon@gmail.com>
2016-03-14 23:23:33 +0900

    UserApiController 변경 및 EntityNotFoundMapper 추가

---
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/error/EntityNotFoundMapper.java
@@ -0,0 +1,15 @@
+package kr.co.hanbit.mastering.springmvc4.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class EntityNotFoundMapper {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Entity could not be found")
+    public void handleNotFound() {
+    }
+}
--- src/main/java/kr/co/hanbit/mastering/springmvc4/user/api/UserApiController.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/user/api/UserApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.hanbit.mastering.springmvc4.error.EntityNotFoundException;
 import kr.co.hanbit.mastering.springmvc4.user.User;
 import kr.co.hanbit.mastering.springmvc4.user.UserRepository;
 
@@ -41,16 +42,17 @@ public class UserApiController {
     }
 
     @RequestMapping(value = "/user/{email}", method = RequestMethod.PUT)
-    public ResponseEntity<User> updateUser(@PathVariable String email, @RequestBody User user) {
+    public ResponseEntity<User> updateUser(@PathVariable String email, @RequestBody User user)
+            throws EntityNotFoundException {
         if (!userRepository.exists(user.getEmail())) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        User saved = userRepository.save(email, user);
+        User saved = userRepository.update(email, user);
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/user/{email}", method = RequestMethod.DELETE)
-    public ResponseEntity<User> deleteUser(@PathVariable String email) {
+    public ResponseEntity<User> deleteUser(@PathVariable String email) throws EntityNotFoundException {
         if (!userRepository.exists(email)) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
