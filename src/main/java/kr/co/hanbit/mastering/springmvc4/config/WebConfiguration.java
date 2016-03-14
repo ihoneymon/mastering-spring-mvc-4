8755996f35f5969fdea9a88d370927387371766b
honeymon <ihoneymon@gmail.com>
2016-03-14 23:57:43 +0900

    SpringForx: Swagger 의존성 추가

--- src/main/java/kr/co/hanbit/mastering/springmvc4/config/WebConfiguration.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/config/WebConfiguration.java
@@ -22,8 +22,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import kr.co.hanbit.mastering.springmvc4.date.KRLocalDateFormatter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
@@ -68,4 +72,9 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
     }
+
+//    @Bean
+//    public Docket userApi() {
+//        return new Docket(DocumentationType.SWAGGER_2).select().paths(path -> path.startsWith("/api/")).build();
+//    }
 }
