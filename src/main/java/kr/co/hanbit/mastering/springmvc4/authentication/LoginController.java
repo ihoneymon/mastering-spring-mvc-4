4a3e5b9388bfcd6b19abfece23d689ecc82c28ed
honeymon <ihoneymon@gmail.com>
2016-03-17 19:42:17 +0900

    접근 사용자 권한 표시하는 부분에 대한 조건 추가

---
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/authentication/LoginController.java
@@ -0,0 +1,13 @@
+package kr.co.hanbit.mastering.springmvc4.authentication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String authenticate() {
+        return "login";
+    }
+}
