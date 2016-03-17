ad05d5af639614809325d8dd3d3e67a80f09f284
honeymon <ihoneymon@gmail.com>
2016-03-18 08:00:44 +0900

    /signup, /signin 에 대한 접근허용

--- src/main/java/kr/co/hanbit/mastering/springmvc4/config/WebSecurityConfiguration.java
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/config/WebSecurityConfiguration.java
@@ -16,7 +16,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
             .logout().logoutSuccessUrl("/login")
             .and()
-            .authorizeRequests().antMatchers("/webjars/**", "/login").permitAll()
+            .authorizeRequests()
+            .antMatchers("/webjars/**", "/login", "/signin/**", "/signup").permitAll()
             .anyRequest().authenticated();
     }
 }
