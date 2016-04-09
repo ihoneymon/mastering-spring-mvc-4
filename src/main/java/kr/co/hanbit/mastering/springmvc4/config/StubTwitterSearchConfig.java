bb8905b49a71efd8f78a61f4e7cd56ee9b299636
honeymon <ihoneymon@gmail.com>
2016-04-09 15:17:05 +0900

    SearchController에 대한 Stub 테스트 작성

---
+++ src/main/java/kr/co/hanbit/mastering/springmvc4/config/StubTwitterSearchConfig.java
@@ -0,0 +1,20 @@
+package kr.co.hanbit.mastering.springmvc4.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import kr.co.hanbit.mastering.springmvc4.search.LightTweet;
+import kr.co.hanbit.mastering.springmvc4.search.TwitterSearch;
+
+@Configuration
+public class StubTwitterSearchConfig {
+
+    @Primary
+    @Bean
+    public TwitterSearch twitterSearch() {
+        return (searchType, keywords) -> Arrays.asList(new LightTweet("tweetText"), new LightTweet("secondTweet"));
+    }
+}
